19702f5822e11324a8484bfb7cec3ccc21611d6d
glovermoose <elzilcho@inbox.com>
2016-04-23 10:35:57 -0700

    basic shooter

---
+++ shooter/js/main.js
@@ -0,0 +1,26 @@
+﻿
+function Draw()
+{
+    CONTEXT.clearRect(0, 0, CANVAS.width, CANVAS.height);
+    player.Draw();
+}
+
+
+function Update()
+{
+    player.Update();
+}
+function loop()
+{
+    Update();
+    Draw();
+
+}
+
+setInterval(function ()
+{
+    GetDealtaTime();
+
+    loop();
+}
+, 1000 / Time.FPS)
