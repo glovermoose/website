7b806e0f5c8cbbb550751d908ffa042b0211b1d7
glovermoose <elzilcho@inbox.com>
2016-05-09 19:02:19 +1000

    madeImageTiny

--- Scripts/_references.js
+++
@@ -1,5 +0,0 @@
-﻿/// <autosync enabled="true" />
-/// <reference path="../js/assets.js" />
-/// <reference path="../js/main.js" />
-/// <reference path="../js/objects.js" />
-/// <reference path="../js/scene.js" />
